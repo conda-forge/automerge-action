57b6b78c35e3eefc3b7641b40b23a6a91169f9f5
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-01-15 14:41:51 -0600

    ENH added pkg skeleton (#16)
    * ENH added pkg skeleton
    
    * ENH contents of python setup

---
+++ conda_forge_tick_action/tests/test_exists.py
@@ -0,0 +1,7 @@
+"""
+test to make sure things are running
+"""
+
+
+def test_exists():
+    assert True
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="conda_forge_tick_action",
+    version="0.1",
+    packages=find_packages(),
+    # will add later
+    # entry_points={
+    #     'console_scripts': [
+    #         'run-regro-cf-autotick-bot-action=conda_forge_tick_action.__main__:main',
+    #     ],
+    # },
+)
