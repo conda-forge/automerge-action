df59aec891a272a70d0f9188153ee15035969852
Matthew R Becker <becker.mr@gmail.com>
2020-04-30 13:58:44 -0500

    ENH add rest of bad states

--- conda_forge_tick_action/automerge.py
+++ conda_forge_tick_action/automerge.py
@@ -18,7 +18,7 @@ IGNORED_STATUSES = []
 
 # sets of states that indicate good / bad / neutral in the github API
 NEUTRAL_STATES = ['pending']
-BAD_STATES = ['failure', 'error']
+BAD_STATES = ['failure', 'error', 'action_required', 'canceled', 'timed_out', 'failed']
 GOOD_MERGE_STATES = ["clean", "has_hooks", "unknown", "unstable"]
 
 
