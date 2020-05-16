2d62d861b7b0b280cecca9edc7340d823ed7d141
Matthew R Becker <becker.mr@gmail.com>
2020-05-16 17:34:54 -0500

    PROD setup dev version

--- conda_forge_automerge_action/tests/test_exists.py
+++
@@ -1,7 +0,0 @@
-"""
-test to make sure things are running
-"""
-
-
-def test_exists():
-    assert True
--- conda_forge_automerge_action/tests/test_only_bot.py
+++
@@ -1,5 +0,0 @@
-from ..automerge import ALLOWED_USERS
-
-
-def test_only_bot():
-    assert ALLOWED_USERS == ['regro-cf-autotick-bot']
